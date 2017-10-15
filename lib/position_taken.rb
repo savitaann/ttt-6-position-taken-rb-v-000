7c933e75aebc790fa35fec60224d50fe98345692
savita madray <savitamadray01@gmail.com>
2017-10-15 02:05:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,4 @@
 def position_taken?(board, index)
-
   end
 
 
-def position_taken?(board, index)
-  if board[0] != [" "]
-    false
-  end
-end
-
-def position_taken?(board, index)
-  if board[0] != [""]
-      false
-  end
-end
